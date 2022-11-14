ad2150bc56f7b371ff14fda098a4d3e58a548a5d
SalihaKoc <salihakc90@gmail.com>
2022-11-14 17:10:09 +0300

    14.11

---
+++ src/J32_Abstract/abstract01/Lastik.java
@@ -0,0 +1,12 @@
+package J32_Abstract.abstract01;
+
+public abstract class  Lastik extends Honda{
+
+    public  abstract  void lastikEbat();//abs method
+
+
+    public void kisLastik(){//concerete method
+
+        System.out.println("agam pis cezası var ihmal etme :)");
+    }
+}
